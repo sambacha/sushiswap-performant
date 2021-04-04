e2d175de8b125df74b9f394a9124d47c57a8a590
sam <sam@freighttrust.com>
2021-04-03 20:25:34 -0700

    inital commit
    Signed-off-by: sam <sam@freighttrust.com>

---
+++ src/components/swap/AdvancedSwapDetailsSection.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {
+  AdvancedSwapDetailsFixed,
+  AdvancedSwapDetailsProps,
+} from './AdvancedSwapDetailsFixed';
+
+export default function AdvancedSwapDetailsSection({
+  trade,
+  ...rest
+}: AdvancedSwapDetailsProps) {
+  return <AdvancedSwapDetailsFixed {...rest} trade={trade ?? undefined} />;
+}
